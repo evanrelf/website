3a386b98040047bd66c661a04e336288a9ad2e2f
Evan Relf <evan@evanrelf.com>
2021-01-01 15:13:15 -0800

    Update pinned Nixpkgs

--- nixpkgs.nix
+++ nixpkgs.nix
@@ -1,11 +1,12 @@
 args:
 
 let
-  # master on 2020-05-27
-  rev = "f0c4c0bc7b8b459efa2aef5b6c322ac77ec344ba";
+  # master on 2020-12-28
+  rev = "23adfcf87dc8d7863335ab181f1b44409205f0a2";
   nixpkgs = builtins.fetchTarball {
     url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
-    sha256 = "0qrsb67fswjgh2gwlxkhzgqzw5i1jm4xbm72fcriqadik4n92rd5";
+    sha256 = "1b76za07pgvw760kynk1qq9cx4a524zxkmd92jr1cks5lsab5r13";
   };
+
 in
   import nixpkgs ({ config = {}; } // args)
